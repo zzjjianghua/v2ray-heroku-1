bbc4d00c2693133732e2ddbcf78b9bddf18dda30
polaris <2186759+xuiv@users.noreply.github.com>
2021-08-20 15:11:49 +0800

    Update colab_proxy.sh

--- colab_proxy.sh
+++ colab_proxy.sh
@@ -20,6 +20,8 @@ sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
 sudo adduser google chrome-remote-desktop
 } &> /dev/null &&
+printf "\nChrome Setup Complete " >&2 ||
+printf "\nChrome Error Occured " >&2
 
 {
 sudo curl -s -L -o /usr/bin/ray https://github.com/xuiv/v2ray-heroku/releases/download/1.01/ray-linux
@@ -27,5 +29,7 @@ sudo curl -s -L -o /usr/bin/config.json https://github.com/xuiv/v2ray-heroku/rel
 sudo chmod +x /usr/bin/ray
 sudo sed -i 's/env:PORT/80/g' /usr/bin/config.json
 } &> /dev/null &&
+printf "\nV2ray Setup Complete " >&2 ||
+printf "\nV2ray Error Occured " >&2
 echo ` curl https://www.trackip.net/i 2> /dev/null |sed -r 's#[^0-9]*([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}).*#\1#' |sed -r '/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/!d' |sed -n '1p' `
 ray &
