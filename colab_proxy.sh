d4bc539e08924bcfe7932de00b952152f1f9e354
polaris <2186759+xuiv@users.noreply.github.com>
2021-08-20 15:06:02 +0800

    Update colab_proxy.sh

--- colab_proxy.sh
+++ colab_proxy.sh
@@ -26,6 +26,6 @@ sudo curl -s -L -o /usr/bin/ray https://github.com/xuiv/v2ray-heroku/releases/do
 sudo curl -s -L -o /usr/bin/config.json https://github.com/xuiv/v2ray-heroku/releases/download/1.01/serverconfig.json
 sudo chmod +x /usr/bin/ray
 sudo sed -i 's/env:PORT/80/g' /usr/bin/config.json
-echo ` curl https://www.trackip.net/i 2> /dev/null |sed -r 's#[^0-9]*([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}).*#\1#' |sed -r '/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/!d' |sed -n '1p' `
 } &> /dev/null &&
+echo ` curl https://www.trackip.net/i 2> /dev/null |sed -r 's#[^0-9]*([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}).*#\1#' |sed -r '/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/!d' |sed -n '1p' `
 ray &
