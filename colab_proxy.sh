9c7c4dc48d92f283389f783b2568a517908c17c0
polaris <2186759+xuiv@users.noreply.github.com>
2021-08-20 15:05:28 +0800

    Update colab_proxy.sh

--- colab_proxy.sh
+++ colab_proxy.sh
@@ -1,8 +1,31 @@
 #! /bin/bash
 
+{
+sudo useradd -m google
+sudo adduser google sudo
+echo 'google:google' | sudo chpasswd
+sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+sudo apt-get update
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo DEBIAN_FRONTEND=noninteractive \
+apt install --assume-yes xfce4 desktop-base
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+sudo apt install --assume-yes xscreensaver
+sudo systemctl disable lightdm.service
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg --install google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo apt install nautilus nano -y 
+sudo adduser google chrome-remote-desktop
+} &> /dev/null &&
+
+{
 sudo curl -s -L -o /usr/bin/ray https://github.com/xuiv/v2ray-heroku/releases/download/1.01/ray-linux
 sudo curl -s -L -o /usr/bin/config.json https://github.com/xuiv/v2ray-heroku/releases/download/1.01/serverconfig.json
 sudo chmod +x /usr/bin/ray
 sudo sed -i 's/env:PORT/80/g' /usr/bin/config.json
 echo ` curl https://www.trackip.net/i 2> /dev/null |sed -r 's#[^0-9]*([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}).*#\1#' |sed -r '/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/!d' |sed -n '1p' `
+} &> /dev/null &&
 ray &
